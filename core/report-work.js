9ed1396d761ab0471a584deedc36330415fa4e12
Salomon Orrego Martinez <sorregom@psl.com.co>
2018-04-09 12:40:27 -0500

    adding logic to get the week of the sprint

--- core/report-work.js
+++ core/report-work.js
@@ -1,4 +1,5 @@
 const jiraToReport = require("./jira-reports-file.json");
+const { getColombiaHolidaysByYear } = require('colombia-holidays');
 
 const currentDate = new Date();
 
@@ -13,13 +14,43 @@ const weekdays = [
 ];
 
 const today = weekdays[currentDate.getDay() - 1];
-const week = getWeek();
+const week = getWeekNumber();
+
+const getWeekNumber = () => {
+  const startingDate = getStartDateFromJiraFile();
+  let diff =(new Date.getTime() - startingDate.getTime()) / 1000;
+  diff /= (60 * 60 * 24 * 7);
+  let weekNumber = Math.abs(Math.round(diff));
+  let sprintWeek = 
+    Math.abs(
+      Math.floor(weekNumber/getNumberOfWeeksInSprint())
+    ) + 1;
+  return 'week' + sprintWeek;
+};
+
+const getStartDateFromJiraFile = () => {
+  let splitDates = jiraToReport.firstDayToReportAt.split("/");
+  const day = Number(splitDates[0]);
+  const month = Number(splitDates[1]) - 1;
+  const year = Number(splitDates[2]);
+  return new Date(year, month, day);
+};
 
 const getTimeToReportAt = (time) => {
   const hour = Number(time.split("h")[0]);
   const minutesInString = time.split("h ")[1];
   const minutes = Number(minutesInString.split("m")[0]);
   return currentDate.setHours(hour, minutes);
+};
+
+const getNumberOfWeeksInSprint = () => {
+  const jiraFileProperties = Object.getOwnPropertyNames(jiraToReport);
+  let totalWeeks = 0;
+
+  jiraFileProperties.forEach((propertie) => {
+    if (propertie.includes("week")){ totalWeeks++ };
+  });
+  return totalWeeks;
 };
 
 const getIssues = () => {
@@ -29,17 +60,18 @@ const getIssues = () => {
   {
     if (issue.timeSpentJIRA !== "" && issue.timeSpentJIRA !== undefined){
       possibleWork[issue].timeSpentJIRA = issue.timeSpentJIRA;
+
       if (issue.comment !== "" && issue.comment !== undefined){
         possibleWork[issue].comment = issue.comment;
+
+        if (issue.hasOwnProperty(startTime)){
+          possibleWork[issue].startDate = getTimeToReportAt(issue.startTime);
+        }
+        else {
+          possibleWork[issue].startDate = currentDate.setHours(8, 0);
+        }
+        issues.push(possibleWork[issue]);
       }
-  
-      if (issue.hasOwnProperty(startTime)){
-        possibleWork[issue].startDate = getTimeToReportAt(issue.startTime);
-      }
-      else {
-        possibleWork[issue].startDate = currentDate.setHours(8, 0);
-      }
-      issues.push(possibleWork[issue]);
     }
   }
   return issues;
