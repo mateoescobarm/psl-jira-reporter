94d60c0925fcffe48bbbe75ba7aeed5c1e4028d4
mateoescobarm <mateoescobarm@gmail.com>
2021-02-09 11:28:55 -0500

    update project name

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const { getColombiaHolidaysByYear } = require('colombia-holidays');
 const moment = require('moment');
 
 const currentDate = moment().utcOffset(-5);
-const project = 'Kinesso Optimization - Kinesso';
+const project = 'Kinesso Outcome Planner - Kinesso';
 const place = 'Sede PSL'
 
 const daily = {
